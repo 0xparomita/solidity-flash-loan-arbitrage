05ab5ae424cd1b9f537c230be362a8031734b3b5
Parmita <160781867+0xparomita@users.noreply.github.com>
2026-02-24 20:07:57 +0600

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/YOUR_API_KEY",
+        blockNumber: 19000000
+      }
+    }
+  }
+};
